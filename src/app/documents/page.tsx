dbc319c3bd38620dd322595a6702928ffbd3c957
DenVlasParviz <vlas8366@gmail.com>
2025-09-08 17:21:59 +0300

    Init commit

---
+++ src/app/documents/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const DocumentPage = () => {
+    return (
+        <div>
+            Click <Link href="/documents/123">Here</Link> to go to documentId
+
+        </div>
+    )
+}
+export default DocumentPage
